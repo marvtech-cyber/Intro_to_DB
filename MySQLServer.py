b7bd29cf614b900ba1591dc426802e182bd81b80
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-26 16:16:52 +0300

    database setup

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,36 +1,30 @@
 import mysql.connector
 
-"""
-Write a simple python script that creates the database alx_book_store in your MySQL server.
+def create_database():
+    try:
+        # Establish a connection to the MySQL server
+        db = mysql.connector.connect(
+            host="localhost",
+            user="your_username",
+            password="your_password"
+        )
 
-Name of python script should be MySQLServer.py
-If the database alx_book_store already exists, your script should not fail
-You are not allowed to use the SELECT or SHOW statements
+        # Create a cursor object
+        cursor = db.cursor()
 
-"""
+        # Create the database if it doesn't exist
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-try:
-    # Establish a connection to the MYSQL server
-    db = mysql.connector.connect(
-        host = "localhost",
-        user = "your_username",
-        password = "your_password"
-    )
+        # Print a success message
+        print("Database 'alx_book_store' created successfully!")
 
-    # Create a cursor object
-    cursor = db.cursor()
+        # Close the cursor and connection
+        cursor.close()
+        db.close()
 
-    # Create the database if it doesn't exist
-    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    except mysql.connector.Error as err:
+        # Print an error message if there's an issue connecting to the DB
+        print("Error connecting to the database:", err)
 
-    # Print a success message
-    print("Database 'alx_book_store' created successfully!")
- 
-
-except mysql.connector.Error as err:
-    # Print an error message
-    print("Error connecting to the database", err)
-
-# Close the cursor and connection
-    cursor.close()
-    db.close()
+if __name__ == "__main__":
+    create_database()
