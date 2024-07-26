bd83c60bc8544043910903716edc707c8db49d92
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-26 15:44:21 +0300

    changed foreign key setup

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,5 +2,5 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books(book_id PRIMARY KEY, title VARCHAR (130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
 CREATE TABLE Authors(author_id PRIMARY KEY, author_name VARCHAR (215));
 CREATE TABLE Customers(customer_id PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
-CREATE TABLE Orders(order_id PRIMARY KEY, customer_id FOREIGN KEY, order_date DATE);
+CREATE TABLE Orders(order_id INT PRIMARY KEY, customer_id INT FOREIGN KEY(customer_id), order_date DATE);
 CREATE TABLE Order_Details(order_detail_id PRIMARY KEY, order_id FOREIGN KEY, book_id FOREIGN KEY, quantity DOUBLE);
