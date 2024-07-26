52d9d8d9bb4c193518be257a63f1c0d420f4e2d9
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-26 16:26:06 +0300

    tables creation scripts

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,5 @@
-SELECT * FROM Books;
-SELECT * FROM Authors;
-SELECT * FROM Customers;
-SELECT * FROM Orders;
-SELECT * FROM Order_Details;
+CREATE TABLE Books(book_id PRIMARY KEY, title VARCHAR (130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
+CREATE TABLE Authors(author_id PRIMARY KEY, author_name VARCHAR (215));
+CREATE TABLE Customers(customer_id PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
+CREATE TABLE Orders(order_id INT PRIMARY KEY, customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE);
+CREATE TABLE Order_Details(order_detail_id PRIMARY KEY, order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id), book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE);
