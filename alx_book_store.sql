6761d43c3cedc5b38ef1c4331f16a11e49143d16
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-26 15:38:58 +0300

    added database creation statement

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books(book_id PRIMARY KEY, title VARCHAR (130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
 CREATE TABLE Authors(author_id PRIMARY KEY, author_name VARCHAR (215));
 CREATE TABLE Customers(customer_id PRIMARY KEY, customer_name VARCHAR (215), email VARCHAR (215), address TEXT);
